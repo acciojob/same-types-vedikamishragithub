3666c34755cc37d4e0c9575e6ae71b583a5ea0a5
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-02-14 10:38:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function isSameType(value1, value2) {
 	if(Number.isNaN(value1) && Number.isNaN(value2)){
 		return true;
-	}
+	}else if ((Number.isNaN(value1) && !Number.isNaN(value2)) || (!Number.isNaN(value1) && Number.isNaN(value2))){
+    return false;
+    } 
 	else if(typeof value1 === typeof value2){
 		return true;
 	}else{
@@ -10,6 +12,6 @@ function isSameType(value1, value2) {
 }
 
 // do not change the code below.
-let value1 = prompt("Enter Start of the Range.");
-let value2 = prompt("Enter End Of the Range.");
-alert(isSameType(value1, value2));
+// let value1 = prompt("Enter Start of the Range.");
+// let value2 = prompt("Enter End Of the Range.");
+// alert(isSameType(value1, value2));
